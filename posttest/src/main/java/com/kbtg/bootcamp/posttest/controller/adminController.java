b96489d1d362733739b2341455c25d2647dbae7f
silversky <nopphadon.phanwong@gmail.com>
2024-02-25 16:48:42 +0700

    Create unit testing for admin.

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/controller/adminController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/controller/adminController.java
@@ -5,20 +5,18 @@ import com.kbtg.bootcamp.posttest.Service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class adminController {
+public class AdminController {
 
     @Autowired
     AdminService adminService;
-    
+
     @PostMapping("/admin/lotteries")
-    public ResponseEntity createLotteriesTicket(@RequestBody CreateLotteryRequest createLotteryRequest){
-//        adminService.createLottery(createLotteryRequest);
+    public ResponseEntity CreateLotteriesTicket(@RequestBody CreateLotteryRequest createLotteryRequest) {
         return new ResponseEntity(adminService.createLottery(createLotteryRequest), HttpStatus.CREATED);
     }
 }
---
+++ posttest/src/test/java/com/kbtg/bootcamp/posttest/AdminControllerTest.java
@@ -0,0 +1,40 @@
+package com.kbtg.bootcamp.posttest;
+
+
+import com.kbtg.bootcamp.posttest.Controller.AdminController;
+import com.kbtg.bootcamp.posttest.Request.CreateLotteryRequest;
+import com.kbtg.bootcamp.posttest.Response.CreateLotteryResponse;
+import com.kbtg.bootcamp.posttest.Service.AdminService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.Mockito.when;
+
+class AdminControllerTest {
+
+    @InjectMocks
+    AdminController adminController;
+
+    @Mock
+    AdminService adminService;
+
+    @Test
+    void createLotteriesTicket() {
+        CreateLotteryRequest request = new CreateLotteryRequest();
+        request.setTicket("12345");
+        request.setAmount(1);
+        request.setPrice(80L);
+        CreateLotteryResponse response = CreateLotteryResponse.builder().ticket("12345").build();
+        ResponseEntity<CreateLotteryResponse> responseResponseEntity = new ResponseEntity<>(response, HttpStatus.CREATED);
+
+        when(adminService.createLottery(request)).thenReturn(response);
+        when(adminController.CreateLotteriesTicket(request)).thenReturn(responseResponseEntity);
+
+        ResponseEntity<CreateLotteryResponse> testResponse = adminController.CreateLotteriesTicket(request);
+        Assertions.assertEquals(HttpStatus.CREATED, testResponse.getStatusCode());
+    }
+}
